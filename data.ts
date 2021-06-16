50a35a67f3576ce2a3e15e24df5aab35c5d6f93c
unknown <--unset>
2021-06-16 23:27:03 +0900

    Render text with color

--- data.ts
+++ data.ts
@@ -5,17 +5,62 @@ export type Circle = {
         y: number;
     };
     radius: number;
+    color: {
+        r: number;
+        g: number;
+        b: number;
+        a: number;
+    };
 }
 
-export type DataList = Array<Circle>;
+export type Text = {
+    type: 'text';
+    align: 'left' | 'center' | 'right';
+    position: {
+        x: number;
+        y: number;
+    };
+    fontSize: number;
+    content: string;
+    color: {
+        r: number;
+        g: number;
+        b: number;
+        a: number;
+    };
+}
+
+export type DataList = Array<Circle | Text>;
 
 export const dataList: DataList = [
     {
         type: "circle",
         center: {
-            x: 50,
-            y: 50,
+            x: 150,
+            y: 150,
         },
-        radius: 20,
+        radius: 100,
+        color: {
+            r: 255,
+            g: 0,
+            b: 0,
+            a: 1,
+        },
+    },
+    {
+        type: "text",
+        align: 'right',
+        fontSize: 10,
+        position: {
+            x: 250,
+            y: 150,
+        },
+        content: "hello world",
+        color: {
+            r: 0,
+            g: 255,
+            b: 0,
+            a: 1,
+        },
     },
 ];
--- render.ts
+++ render.ts
@@ -1,10 +1,19 @@
-import { Circle, DataList } from "./data";
+import { Circle, DataList, Text } from "./data";
 
 export function render(context: CanvasRenderingContext2D, dataList: DataList) {
     dataList.forEach(data => {
         switch (data.type) {
             case "circle": {
-                return renderCircle(context, data);
+                renderCircle(context, data);
+                return;
+            }
+            case 'text': {
+                renderText(context, data);
+                return;
+            }
+            default: {
+                console.error(`unknown data ${data}`);
+                return;
             }
         }
     });
@@ -21,5 +30,13 @@ function renderCircle(context: CanvasRenderingContext2D, data: Circle): void {
         0,
         2 * Math.PI);
     context.closePath();
+    context.fillStyle = `rgba(${data.color.r}, ${data.color.g}, ${data.color.b}, ${data.color.a})`;
     context.fill();
+}
+
+function renderText(context: CanvasRenderingContext2D, data: Text) {
+    context.textAlign = data.align,
+    context.font = `${data.fontSize}px`;
+    context.fillStyle = `rgba(${data.color.r}, ${data.color.g}, ${data.color.b}, ${data.color.a})`;
+    context.fillText(data.content, data.position.x, data.position.y);
 }
